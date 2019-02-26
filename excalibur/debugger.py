a78f7ced417d0dbb89f87fd672b17ae2c3aa9ea8
ceacar <alex.yonder@yahoo.com>
2019-02-25 21:52:53 -0500

    added debugger

--- excalibur/debugger.py
+++ excalibur/debugger.py
@@ -4,7 +4,7 @@ def debug(func):
         ag = [item for item in args]
         kg = [item for item in kwargs]
         sys.stderr.write(
-            ">{func_name} with args:{ag}, kwargs:{kg}"\
+            ">{func_name} with args:{ag}, kwargs:{kg}\n"\
             .format(
                 func_name = func.__qualname__,
                 ag = ag, kg = kg
