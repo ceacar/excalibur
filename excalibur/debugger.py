169cefd9c24b158171f698c14e4ca5888fb4e5a0
ceacar <alex.yonder@yahoo.com>
2019-02-26 21:45:32 -0500

    added debug_with_prefix

--- excalibur/debugger.py
+++ excalibur/debugger.py
@@ -1,5 +1,7 @@
 import sys
+from functools import wraps
 def debug(func):
+    @wraps(func)
     def wrap(*args, **kwargs):
         ag = [item for item in args]
         kg = [item for item in kwargs]
@@ -12,3 +14,21 @@ def debug(func):
         )
         func(*args, **kwargs)
     return wrap
+
+def debug_with_prefix(prefix):
+    def debug(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            ag = [item for item in args]
+            kg = [item for item in kwargs]
+            sys.stderr.write(
+                "{prefix}{func_name} with args:{ag}, kwargs:{kg}\n"\
+                .format(
+                    prefix = prefix,
+                    func_name = func.__qualname__,
+                    ag = ag, kg = kg
+                )
+            )
+            func(*args, **kwargs)
+        return wrap
+
