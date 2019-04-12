d14ffe6a310b1fb3e7940ac6bae88a6fd4a55866
ceacar <alex.yonder@yahoo.com>
2019-04-12 14:56:33 -0400

    updated logger now works perfectly

--- excalibur/logger.py
+++ excalibur/logger.py
@@ -1,16 +1,21 @@
 import logging
-logging.basicConfig(level = logging.DEBUG)
+
+__logger_instance = None
+
+def __get_singleton_stdout_logger():
+    if not __logger_instance:
+        __logger_instance = get_stdout_logger()
+    return __logger_instance
 
 def logger_wrapper(orig_func):
     """
     a wrapper used as @logger_wrapper to print log of a function
     """
-    import logging
-    logging.basicConfig(filename='/tmp/{}.log'.format(orig_func.__name__), level=logging.INFO)
-
+    #logging.basicConfig(filename='/tmp/{}.log'.format(orig_func.__name__), level=logging.INFO)
+    logger = __get_singleton_stdout_logger()
     @wraps(orig_func)
     def wrapper(*args, **kwargs):
-        logging.info(
+        logger.info(
             'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
         return orig_func(*args, **kwargs)
 
@@ -18,8 +23,10 @@ def logger_wrapper(orig_func):
 
 __default_logging_format = '%(asctime)s|%(levelname)s|%(filename)s|%(module)s|%(funcName)s|%(message)s'
 
-def __get_logger(file_handler):
+def __get_logger(file_handler, level = logging.DEBUG):
     logger = logging.getLogger(__name__) #if run in main the name would be main, module then module
+    #set to DEBUG for root logger by default, so sub logger can have freedom of configuring levels
+    logger.setLevel(level)
     logger.addHandler(file_handler)
     return logger
 
@@ -32,10 +39,9 @@ def get_stdout_logger(logging_level = logging.DEBUG, logger_format = __default_l
     ...
     """
     stream_handler = logging.StreamHandler() #use this as the same of file handler
-    stream_handler.setLevel(logging_level)
     formatter = logging.Formatter(logger_format)
     stream_handler.setFormatter(formatter)
-    return __get_logger(stream_handler)
+    return __get_logger(stream_handler, level = logging_level)
 
 def get_file_logger(log_file_path = '/tmp/logger.log', logging_level = logging.DEBUG, logger_format = __default_logging_format):
     """
@@ -46,8 +52,7 @@ def get_file_logger(log_file_path = '/tmp/logger.log', logging_level = logging.D
     ...
     """
     file_handler= logging.FileHandler(log_file_path) #use this as the same of file handler
-    file_handler.setLevel(logging_level)
     formatter = logging.Formatter(logger_format)
     file_handler.setFormatter(formatter)
-    return __get_logger(file_handler)
+    return __get_logger(file_handler, level = logging_level)
 
