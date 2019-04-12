c512102e95fd8c3d1c7d40f397ddd835792aaffb
ceacar <alex.yonder@yahoo.com>
2019-04-12 14:41:23 -0400

    updated logger

--- excalibur/logger.py
+++ excalibur/logger.py
@@ -1,5 +1,10 @@
+import logging
+logging.basicConfig(level = logging.DEBUG)
 
-def my_logger(orig_func):
+def logger_wrapper(orig_func):
+    """
+    a wrapper used as @logger_wrapper to print log of a function
+    """
     import logging
     logging.basicConfig(filename='/tmp/{}.log'.format(orig_func.__name__), level=logging.INFO)
 
@@ -10,4 +15,39 @@ def my_logger(orig_func):
         return orig_func(*args, **kwargs)
 
     return wrapper
+
+__default_logging_format = '%(asctime)s|%(levelname)s|%(filename)s|%(module)s|%(funcName)s|%(message)s'
+
+def __get_logger(file_handler):
+    logger = logging.getLogger(__name__) #if run in main the name would be main, module then module
+    logger.addHandler(file_handler)
+    return logger
+
+def get_stdout_logger(logging_level = logging.DEBUG, logger_format = __default_logging_format):
+    """
+    usage
+    new_logger = get_stdout_logger()
+    logger.debug("test123") #here need to use logger instad of default logging
+    logger.info("test234") #here need to use logger instad of default logging
+    ...
+    """
+    stream_handler = logging.StreamHandler() #use this as the same of file handler
+    stream_handler.setLevel(logging_level)
+    formatter = logging.Formatter(logger_format)
+    stream_handler.setFormatter(formatter)
+    return __get_logger(stream_handler)
+
+def get_file_logger(log_file_path = '/tmp/logger.log', logging_level = logging.DEBUG, logger_format = __default_logging_format):
+    """
+    usage
+    new_logger = get_file_logger()
+    logger.debug("test123") #here need to use logger instad of default logging
+    logger.info("test234") #here need to use logger instad of default logging
+    ...
+    """
+    file_handler= logging.FileHandler(log_file_path) #use this as the same of file handler
+    file_handler.setLevel(logging_level)
+    formatter = logging.Formatter(logger_format)
+    file_handler.setFormatter(formatter)
+    return __get_logger(file_handler)
 
