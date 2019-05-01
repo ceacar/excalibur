0feae38f46b12dea39d4d31780ceefa14a2d7e26
ceacar <alex.yonder@yahoo.com>
2019-05-01 15:49:43 -0400

    fixed a bug

--- excalibur/requests_util.py
+++ excalibur/requests_util.py
@@ -5,7 +5,7 @@ __requests_timestamp_format = '%a, %d %b %Y %H:%M:%S %Z'
 
 
 def get_remote_file_header(url, auth=None):
-    header_info = requests.head(url, auth)
+    header_info = requests.head(url, auth=auth)
     return header_info
 
 
@@ -13,7 +13,7 @@ def get_file_timestamp(url, auth=None):
     """
     get remote file timestamp, returns a datetime obj
     """
-    header_info = get_remote_file_header(url, auth)
+    header_info = get_remote_file_header(url, auth=auth)
     mod_time = header_info['Last-Modified']
     dt_obj = datetime.datetime.strptime(mod_time, __requests_timestamp_format)
     return dt_obj
