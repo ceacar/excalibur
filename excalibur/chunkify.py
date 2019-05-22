d4127532977d29f679f785477054b3eb655ae44f
ceacar <alex.yonder@yahoo.com>
2019-05-22 11:27:47 -0400

    chunkify.py updated with added file split ability

--- excalibur/chunkify.py
+++ excalibur/chunkify.py
@@ -5,9 +5,9 @@ import random
 import string
 import sys
 
+
 __nproc = mp.cpu_count()
 
-file_name = "EQY_US_ALL_TRADE_ADMIN_20180202.txt"
 
 def _process_wrapper(input_file_name, chunkStart, chunkSize, process_func):
     with open(input_file_name, 'r') as f:
@@ -17,9 +17,18 @@ def _process_wrapper(input_file_name, chunkStart, chunkSize, process_func):
             process_func(line)
         return "{start}-{end} processed".format(start = chunkStart, end = chunkStart + chunkSize)
 
+
+def _save_chunks(input_file_name, chunkStart, chunkSize, process_func, output_file_name):
+    with open(input_file_name, 'r') as f:
+        f.seek(chunkStart)
+        lines = f.read(chunkSize).splitlines()
+        for line in lines:
+            with open(output_file_name, 'w') as output_file:
+                output_file.write(line)
+        return "{start}-{end} written".format(start = chunkStart, end = chunkStart + chunkSize)
+
+
 def _chunkify(fname,size=1024*1024):
-    import pdb
-    pdb.set_trace()
     fileEnd = os.path.getsize(fname)
     with open(fname,'r') as f:
         chunkEnd = f.tell()
@@ -31,6 +40,7 @@ def _chunkify(fname,size=1024*1024):
             yield chunkStart, chunkEnd - chunkStart
             if chunkEnd > fileEnd:
                 break
+
 
 def process_large_file(input_file_name, process_func, size):
     pool = mp.Pool(__nproc)
@@ -49,13 +59,53 @@ def process_large_file(input_file_name, process_func, size):
     #clean up
     pool.close()
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_name", help = "file full abs path with name", type = str) # type is defaulted to str
-    parser.add_argument("size", help = "chunk byte size", type = str) # type is defaulted to str
-    args = parser.parse_args()
 
-    def _print_operation(x):
-        print('processing {0}'.format(x))
-    process_large_file(args.file_name, _print_operation, int(args.size))
+def __pick_file_name(fname, sequence_number):
+    fname_arr = fname.split('.') #  assume file has appendix like a.csv
+    output_file_name = f'{".".join(fname_arr[:-1])}.{sequence_number}.{fname_arr[-1]}'
+    return output_file_name
+
+
+def __save_lines(input_fname, sequence_number, start, read_size):
+    output_file_name = __pick_file_name(input_fname, sequence_number)
+    with open(input_fname, 'r') as ifile:
+        ifile.seek(start)
+        with open(output_file_name, 'w') as wfile:
+            content = ifile.read(read_size)
+            wfile.write(content)
+
+
+def split_large_file(fname, size = 1024*1024):
+    """
+    input:
+    fname(file name), size(size of splitted_file)
+
+    how it works
+    Assume file name is f_name.csv
+    will split file into f_name.0.csv, f_name.1.csv ...
+    """
+    fileEnd = os.path.getsize(fname)
+    sequence_number = 0
+    with open(fname,'r') as f:
+        chunkEnd = f.tell()
+        while True:
+            chunkStart = chunkEnd
+            f.seek(size + chunkStart, 0)
+            f.readline()
+            chunkEnd = f.tell()
+            __save_lines(fname, sequence_number, chunkStart, chunkEnd - chunkStart)
+            if chunkEnd > fileEnd:
+                break
+            sequence_number += 1
+
+#example of how to use multiprocessing
+#if __name__ == '__main__':
+#    import argparse
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("file_name", help = "file full abs path with name", type = str) # type is defaulted to str
+#    parser.add_argument("size", help = "chunk byte size", type = str) # type is defaulted to str
+#    args = parser.parse_args()
+#
+#    def _print_operation(x):
+#        print('processing {0}'.format(x))
+#    process_large_file(args.file_name, _print_operation, int(args.size))
